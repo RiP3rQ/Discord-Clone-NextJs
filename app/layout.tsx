63417cd4154f4fbadb6b2256586ccaf61c1f8791
RiP3rQ <rafalpompa2000@gmail.com>
2023-09-08 18:41:08 +0200

    create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
