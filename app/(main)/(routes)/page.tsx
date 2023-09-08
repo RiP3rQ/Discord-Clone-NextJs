c78830c41300b1a61f2f395f4ce244785d12c893
RiP3rQ <rafalpompa2000@gmail.com>
2023-09-08 10:48:34 +0200

    dark/light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
