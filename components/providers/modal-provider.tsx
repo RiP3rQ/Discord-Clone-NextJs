747a1b87a901cc613ff111297aa5289eb4c538d5
RiP3rQ <rafalpompa2000@gmail.com>
2023-09-09 19:54:00 +0200

    channel creation

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,8 +4,9 @@ import { useEffect, useState } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
-import { EditServerModal } from "../modals/edit-server-modal";
-import { MembersModal } from "../modals/members-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -24,6 +25,7 @@ export const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   );
 };
